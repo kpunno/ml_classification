c6ac7867b6d8219718d030706c07f043814a2d9f
Kristjan Punno <k.punno@gmail.com>
2025-08-03 08:47:57 -0400

    encapsulate file_processing in class

--- file_processing.py
+++ file_processing.py
@@ -12,94 +12,96 @@ import os
 import re
 from typing import Pattern
 
-p_w_upper_bound =0.90
-p_w_lower_bound =0.05
+class FileProcessor:
+    def __init__(self, dirtype, valid_chars=r"^[A-Za-z']+$", stopwords_dir="./stopwords_nltk", p_w_upper_bound=0.95, p_w_lower_bound=0.05):
+        self.p_w_upper_bound = p_w_upper_bound
+        self.p_w_lower_bound = p_w_lower_bound
+        self.dirtype = dirtype
+        self.stopwords = self.get_data(stopwords_dir).split("\n")
+        # Do we want to include numbers? Possibly should be alphanumeric.
+        # Validate tokens (is this the best version?)
+        self.valid_chars = valid_chars
 
-def get_directory(dirtype : str) -> str:
-    dirpath = open(f"./var").read().strip()
-    return f"{dirpath}{dirtype}"
+    def get_directory(self) -> str:
+        dirpath = open(f"./var").read().strip()
+        return f"{dirpath}{self.dirtype}"
 
-def get_files_list(dirtype : str) -> list:
-    dir = get_directory(dirtype)
-    return os.listdir(dir)
+    def get_files_list(self) -> list:
+        dir = self.get_directory()
+        return os.listdir(dir)
 
-def get_files_count(dirtype : str) -> int:
-    return len(get_files_list(dirtype))
+    def get_files_count(self) -> int:
+        return len(self.get_files_list())
 
-# Extract text contents from file
-def get_data(filepath):
-    with open(filepath, "r", errors="ignore") as file:
-        return file.read().lower()
+    # Extract text contents from file
+    def get_data(self, filepath):
+        with open(filepath, "r", errors="ignore") as file:
+            return file.read().lower()
 
-# Do we want to include numbers? Possibly should be alphanumeric.
-# Validate tokens (is this the best version?)
-valid_chars1 = r"^[A-Za-z']+$" 
-stopwords = get_data("./stopwords_nltk").split("\n")
+    # regex full matching
+    def charset_full_match(self, token):
+        return True if re.fullmatch(self.valid_chars, token) else False
 
-# regex full matching
-def charset_full_match(valid_chars_regex, token):
-    return True if re.fullmatch(valid_chars_regex, token) else False
+    # regex partial matching
+    def charset_match(self, token):
+        return True if re.match(self.valid_chars, token) else False
 
-# regex partial matching
-def charset_match(valid_chars_regex, token):
-    return True if re.match(valid_chars_regex, token) else False
+    # print a portion that appears under and over a certain percentage
+    def print_portion(self, tokens : dict, lower_bound_percent : int, upper_bound_percent : int):
+        print("index ".rjust(10), " word".ljust(40), "count".center(10),"percent".center(10),sep="|")
+        print("-"*100)
+        index=1
+        total=0
+        files_count = self.get_files_count()
+        for word,count in tokens.items():
+            rate = count/files_count
+            if lower_bound_percent <= rate <= upper_bound_percent:
+                print(f"{str(index)} ".rjust(10),f" {word}".ljust(40),str(count).center(10),f"{100*count/files_count}%".center(10), sep="|")
+                total+=1
+            index+=1
+        print(f"{total} items appear within {lower_bound_percent*100}% and {upper_bound_percent*100}% of files.")
 
-# print a portion that appears under and over a certain percentage
-def print_portion(tokens : dict, files_count : int, lower_bound_percent : int, upper_bound_percent : int):
-    print("index ".rjust(10), " word".ljust(40), "count".center(10),"percent".center(10),sep="|")
-    print("-"*100)
-    index=1
-    total=1
-    for word,count in tokens.items():
-        rate = count/files_count
-        if lower_bound_percent <= rate <= upper_bound_percent:
-            print(f"{str(index)} ".rjust(10),f" {word}".ljust(40),str(count).center(10),f"{100*count/files_count}%".center(10), sep="|")
-            total+=1
-        index+=1
-    print(f"{total} items appear within {lower_bound_percent*100}% and {upper_bound_percent*100}% of files.")
+    # parse text file, return organized tokens
+    # V2, split lines, clean lines, split lines, validate tokens
+    def parse_text(self, data: str) -> dict:
+        cleaned_tokens=[]
+        line = re.sub(r"\n", " ", data)
+        line = re.sub(r"[.,:\-!]","", line)
+        tokens = line.split(" ")
+        for token in tokens:
+            token = token.strip()
+            if self.charset_full_match(token) and token not in self.stopwords:
+                cleaned_tokens.append(token)
+        return dict.fromkeys(cleaned_tokens, 1)
 
-# parse text file, return organized tokens
-# V2, split lines, clean lines, split lines, validate tokens
-def parse_text(data: str, valid_chars_regex: Pattern) -> dict:
-    cleaned_tokens=[]
-    line = re.sub(r"\n", " ", data)
-    line = re.sub(r"[.,:\-!]","", line)
-    tokens = line.split(" ")
-    for token in tokens:
-        token = token.strip()
-        if charset_full_match(valid_chars_regex, token) and token not in stopwords:
-            cleaned_tokens.append(token)
-    return dict.fromkeys(cleaned_tokens, 1)
+    # build bag of words from scratch
+    def build_dictionary(self) -> dict:
+        dirpath = self.get_directory()
+        files = self.get_files_list()
+        files_count = len(files)
+        print(f"{dirpath} : {files_count}")
 
-# build bag of words from scratch
-def build_dictionary(dirtype : str) -> dict:
-    dirpath = get_directory(dirtype)
-    files_count = get_files_count(dirtype)
-    files = os.listdir(dirpath)
-    files_count = len(files)
-    print(f"{dirpath} : {files_count}")
+        dict_counts = {}
+        for filename in files:
+            filepath = os.path.join(dirpath, filename)
+            data = self.get_data(filepath)
+            token_dict = self.parse_text(data)
+            # Should we use 'keys()' or 'items()'?
+            for word in token_dict.keys():
+                dict_counts[word] = dict_counts.get(word, 0) + 1
 
-    dict_counts = {}
-    for filename in files:
-        filepath = os.path.join(dirpath, filename)
-        data = get_data(filepath)
-        token_dict = parse_text(data, valid_chars1)
-        # Should we use 'keys()' or 'items()'?
-        for word in token_dict.keys():
-            dict_counts[word] = dict_counts.get(word, 0) + 1
-
-    dict_probabilities = {}
-    # type_char = f"p_w|{dirtype[:1]}"
-    type_char = "p_w"
-    for word, count in dict_counts.items():
-        p_w = count/files_count
-        ## Ignore words above, below given probability
-        if p_w_upper_bound >= p_w >= p_w_lower_bound:
-            dict_probabilities[word] = {"count": count, type_char: p_w}
+        dict_probabilities = {}
+        # type_char = f"p_w|{dirtype[:1]}"
+        type_char = "p_w"
+        for word, count in dict_counts.items():
+            p_w = count/files_count
+            ## Ignore words above, below given probability
+            if self.p_w_upper_bound >= p_w >= self.p_w_lower_bound:
+                dict_probabilities[word] = {"count": count, type_char: p_w}
         
 
-    print(f"{len(dict_probabilities)} words in collection.")
-    return dict_probabilities
+        print(f"{len(dict_probabilities)} words in collection.")
+        return dict_probabilities
 
 
 
